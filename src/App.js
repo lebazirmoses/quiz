7826c56287039aa5ebcfe4c6be01d591eb272731
lebazirmoses <lebazir2004@gmail.com>
2024-05-01 22:34:06 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to QuizApp</h1>
+        <p>This is a simple quiz application built with React.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
